983c5bc47b8131633feafcb0f25069518515dbfc
Eric Quinton <eric.quinton@irstea.fr>
2016-06-02 11:55:44 +0200

    Création de la structure de la base de données

--- database/droits_structure_create.sql
+++ database/droits_structure_create.sql
@@ -1,118 +1,139 @@
+create schema gacl;
 
-
-CREATE TABLE sturio.gacl.aclacl (
+CREATE TABLE gacl.aclacl (
                 aclaco_id INTEGER NOT NULL,
                 aclgroup_id INTEGER NOT NULL,
                 CONSTRAINT aclacl_pk PRIMARY KEY (aclaco_id, aclgroup_id)
 );
-COMMENT ON TABLE sturio.gacl.aclacl IS 'Table des droits attribués';
+COMMENT ON TABLE gacl.aclacl IS 'Table des droits attribués';
 
 
-CREATE SEQUENCE sturio.gacl.aclaco_aclaco_id_seq;
+CREATE SEQUENCE gacl.aclaco_aclaco_id_seq;
 
-CREATE TABLE sturio.gacl.aclaco (
-                aclaco_id INTEGER NOT NULL DEFAULT nextval('sturio.gacl.aclaco_aclaco_id_seq'),
+CREATE TABLE gacl.aclaco (
+                aclaco_id INTEGER NOT NULL DEFAULT nextval('gacl.aclaco_aclaco_id_seq'),
                 aclappli_id INTEGER NOT NULL,
                 aco VARCHAR NOT NULL,
                 CONSTRAINT aclaco_pk PRIMARY KEY (aclaco_id)
 );
-COMMENT ON TABLE sturio.gacl.aclaco IS 'Table des droits gérés';
+COMMENT ON TABLE gacl.aclaco IS 'Table des droits gérés';
 
 
-ALTER SEQUENCE sturio.gacl.aclaco_aclaco_id_seq OWNED BY sturio.gacl.aclaco.aclaco_id;
+ALTER SEQUENCE gacl.aclaco_aclaco_id_seq OWNED BY gacl.aclaco.aclaco_id;
 
-CREATE SEQUENCE sturio.gacl.aclappli_aclappli_id_seq;
+CREATE SEQUENCE gacl.aclappli_aclappli_id_seq;
 
-CREATE TABLE sturio.gacl.aclappli (
-                aclappli_id INTEGER NOT NULL DEFAULT nextval('sturio.gacl.aclappli_aclappli_id_seq'),
+CREATE TABLE gacl.aclappli (
+                aclappli_id INTEGER NOT NULL DEFAULT nextval('gacl.aclappli_aclappli_id_seq'),
                 appli VARCHAR NOT NULL,
                 applidetail VARCHAR,
                 CONSTRAINT aclappli_pk PRIMARY KEY (aclappli_id)
 );
-COMMENT ON TABLE sturio.gacl.aclappli IS 'Table des applications gérées';
-COMMENT ON COLUMN sturio.gacl.aclappli.appli IS 'Nom de l''application pour la gestion des droits';
-COMMENT ON COLUMN sturio.gacl.aclappli.applidetail IS 'Description de l''application';
+COMMENT ON TABLE gacl.aclappli IS 'Table des applications gérées';
+COMMENT ON COLUMN gacl.aclappli.appli IS 'Nom de l''application pour la gestion des droits';
+COMMENT ON COLUMN gacl.aclappli.applidetail IS 'Description de l''application';
 
 
-ALTER SEQUENCE sturio.gacl.aclappli_aclappli_id_seq OWNED BY sturio.gacl.aclappli.aclappli_id;
+ALTER SEQUENCE gacl.aclappli_aclappli_id_seq OWNED BY gacl.aclappli.aclappli_id;
 
-CREATE SEQUENCE sturio.gacl.aclgroup_aclgroup_id_seq;
+CREATE SEQUENCE gacl.aclgroup_aclgroup_id_seq;
 
-CREATE TABLE sturio.gacl.aclgroup (
-                aclgroup_id INTEGER NOT NULL DEFAULT nextval('sturio.gacl.aclgroup_aclgroup_id_seq'),
+CREATE TABLE gacl.aclgroup (
+                aclgroup_id INTEGER NOT NULL DEFAULT nextval('gacl.aclgroup_aclgroup_id_seq'),
                 groupe VARCHAR NOT NULL,
                 aclgroup_id_parent INTEGER,
                 CONSTRAINT aclgroup_pk PRIMARY KEY (aclgroup_id)
 );
-COMMENT ON TABLE sturio.gacl.aclgroup IS 'Groupes des logins';
+COMMENT ON TABLE gacl.aclgroup IS 'Groupes des logins';
 
 
-ALTER SEQUENCE sturio.gacl.aclgroup_aclgroup_id_seq OWNED BY sturio.gacl.aclgroup.aclgroup_id;
+ALTER SEQUENCE gacl.aclgroup_aclgroup_id_seq OWNED BY gacl.aclgroup.aclgroup_id;
 
-CREATE SEQUENCE sturio.gacl.acllogin_acllogin_id_seq;
+CREATE SEQUENCE gacl.acllogin_acllogin_id_seq;
 
-CREATE TABLE sturio.gacl.acllogin (
-                acllogin_id INTEGER NOT NULL DEFAULT nextval('sturio.gacl.acllogin_acllogin_id_seq'),
+CREATE TABLE gacl.acllogin (
+                acllogin_id INTEGER NOT NULL DEFAULT nextval('gacl.acllogin_acllogin_id_seq'),
                 login VARCHAR NOT NULL,
                 logindetail VARCHAR NOT NULL,
                 CONSTRAINT acllogin_pk PRIMARY KEY (acllogin_id)
 );
-COMMENT ON TABLE sturio.gacl.acllogin IS 'Table des logins des utilisateurs autorisés';
-COMMENT ON COLUMN sturio.gacl.acllogin.logindetail IS 'Nom affiché';
+COMMENT ON TABLE gacl.acllogin IS 'Table des logins des utilisateurs autorisés';
+COMMENT ON COLUMN gacl.acllogin.logindetail IS 'Nom affiché';
 
 
-ALTER SEQUENCE sturio.gacl.acllogin_acllogin_id_seq OWNED BY sturio.gacl.acllogin.acllogin_id;
+ALTER SEQUENCE gacl.acllogin_acllogin_id_seq OWNED BY gacl.acllogin.acllogin_id;
 
-CREATE TABLE sturio.gacl.acllogingroup (
+CREATE TABLE gacl.acllogingroup (
                 acllogin_id INTEGER NOT NULL,
                 aclgroup_id INTEGER NOT NULL,
                 CONSTRAINT acllogingroup_pk PRIMARY KEY (acllogin_id, aclgroup_id)
 );
-COMMENT ON TABLE sturio.gacl.acllogingroup IS 'Table des relations entre les logins et les groupes';
+COMMENT ON TABLE gacl.acllogingroup IS 'Table des relations entre les logins et les groupes';
 
 
-ALTER TABLE ONLY sturio.gacl.gaclacl ALTER COLUMN note TYPE TEXT, ALTER COLUMN note DROP NOT NULL;
 
-ALTER TABLE ONLY sturio.gacl.gaclacl ALTER COLUMN return_value TYPE TEXT, ALTER COLUMN return_value DROP NOT NULL;
-
-ALTER TABLE sturio.gacl.aclacl ADD CONSTRAINT aclaco_aclacl_fk
+ALTER TABLE gacl.aclacl ADD CONSTRAINT aclaco_aclacl_fk
 FOREIGN KEY (aclaco_id)
-REFERENCES sturio.gacl.aclaco (aclaco_id)
+REFERENCES gacl.aclaco (aclaco_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE sturio.gacl.aclaco ADD CONSTRAINT aclappli_aclaco_fk
+ALTER TABLE gacl.aclaco ADD CONSTRAINT aclappli_aclaco_fk
 FOREIGN KEY (aclappli_id)
-REFERENCES sturio.gacl.aclappli (aclappli_id)
+REFERENCES gacl.aclappli (aclappli_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE sturio.gacl.aclacl ADD CONSTRAINT aclgroup_aclacl_fk
+ALTER TABLE gacl.aclacl ADD CONSTRAINT aclgroup_aclacl_fk
 FOREIGN KEY (aclgroup_id)
-REFERENCES sturio.gacl.aclgroup (aclgroup_id)
+REFERENCES gacl.aclgroup (aclgroup_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE sturio.gacl.aclgroup ADD CONSTRAINT aclgroup_aclgroup_fk
+ALTER TABLE gacl.aclgroup ADD CONSTRAINT aclgroup_aclgroup_fk
 FOREIGN KEY (aclgroup_id_parent)
-REFERENCES sturio.gacl.aclgroup (aclgroup_id)
+REFERENCES gacl.aclgroup (aclgroup_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE sturio.gacl.acllogingroup ADD CONSTRAINT aclgroup_acllogingroup_fk
+ALTER TABLE gacl.acllogingroup ADD CONSTRAINT aclgroup_acllogingroup_fk
 FOREIGN KEY (aclgroup_id)
-REFERENCES sturio.gacl.aclgroup (aclgroup_id)
+REFERENCES gacl.aclgroup (aclgroup_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE sturio.gacl.acllogingroup ADD CONSTRAINT acllogin_acllogingroup_fk
+ALTER TABLE gacl.acllogingroup ADD CONSTRAINT acllogin_acllogingroup_fk
 FOREIGN KEY (acllogin_id)
-REFERENCES sturio.gacl.acllogin (acllogin_id)
+REFERENCES gacl.acllogin (acllogin_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
+
+CREATE TABLE gacl.log
+(
+   log_id       serial        NOT NULL,
+   login        varchar(32)   NOT NULL,
+   nom_module   varchar,
+   log_date     timestamp     NOT NULL,
+   commentaire  varchar
+);
+
+-- Column log_id is associated with sequence gacl.log_log_id_seq
+
+ALTER TABLE gacl.log
+   ADD CONSTRAINT log_pk
+   PRIMARY KEY (log_id);
+
+CREATE INDEX log_date_idx ON gacl.log USING btree (log_date);
+CREATE INDEX log_login_idx ON gacl.log USING btree (login);
+
+
+COMMENT ON TABLE gacl.log IS 'Liste des connexions ou des actions enregistrées';
+COMMENT ON COLUMN gacl.log.log_date IS 'Heure de connexion';
+COMMENT ON COLUMN gacl.log.commentaire IS 'Donnees complementaires enregistrees';
+

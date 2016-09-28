cfc2be5241702916a843a943a8d13c3b51d8cbda
Eric Quinton <eric.quinton@irstea.fr>
2016-09-28 12:36:18 +0200

    Correction de la recherche des identifiants complémentaires

--- modules/classes/objectIdentifier.class.php
+++ modules/classes/objectIdentifier.class.php
@@ -6,7 +6,7 @@
  * Copyright 2016 - All rights reserved
  */
 class ObjectIdentifier extends ObjetBDD {
-	private $sql = "select object_identifier_id, uid, identifier_type_value,
+	private $sql = "select object_identifier_id, uid, object_identifier_value, identifier_type_id,
 					identifier_type_name, identifier_type_code
 					from object_identifier
 					join identifier_type using (identifier_type_id)";
@@ -24,7 +24,11 @@ class ObjectIdentifier extends ObjetBDD {
 						"requis" => 1,
 						"parentAttrib" => 1 
 				),
-				"identifier_type_value" => array (
+				"identifier_type_id" => array (
+						"type" => 1,
+						"requis" => 1 
+				),
+				"object_identifier_value" => array (
 						"type" => 0,
 						"requis" => 1 
 				) 
@@ -33,13 +37,14 @@ class ObjectIdentifier extends ObjetBDD {
 	}
 	/**
 	 * Retourne la liste des identifiants secondaires associes a un UID
-	 * @param unknown $uid
+	 * 
+	 * @param unknown $uid        	
 	 */
 	function getListFromUid($uid) {
 		if ($uid > 0) {
-			$data["uid"] = $uid;
+			$data ["uid"] = $uid;
 			$where = " where uid = :uid";
-			return $this->getListeParamAsPrepared($this->sql, $data);
+			return $this->getListeParamAsPrepared ( $this->sql . $where, $data );
 		}
 	}
 }

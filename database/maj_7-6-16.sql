00d8d0021a07b993fda7e2ad3731b0da07c15c94
Eric Quinton <eric.quinton@irstea.fr>
2016-10-25 12:33:13 +0200

    Mise à niveau pour la version 1.0

--- database/maj_7-6-16.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "eabxcol"."col"."container" DROP CONSTRAINT "container_container_fk";
-
-ALTER TABLE "eabxcol"."col"."container" DROP COLUMN "parent_container_id";
-
-ALTER TABLE "eabxcol"."col"."container" DROP COLUMN "container_range";

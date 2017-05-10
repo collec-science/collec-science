69326993bf0c4f70b74551d5e2cd61a1ba837dee
Eric Quinton <eric.quinton@irstea.fr>
2017-05-10 16:17:20 +0200

    Version 1.0.7 du 10/05/2017
    News et numéro de version

--- param/param.default.inc.php
+++ param/param.default.inc.php
@@ -6,8 +6,8 @@
 * Parametres par defaut de l'application
 * Si des modifications doivent etre apportees, faites-les dans le fichier param.inc.php
 */
-$APPLI_version = "1.0.6A";
-$APPLI_versiondate = "03/04/2017";
+$APPLI_version = "1.0.7";
+$APPLI_versiondate = "10/05/2017";
 $language = "fr";
 $DEFAULT_formatdate = "fr";
 /*

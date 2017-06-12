1d77746d5da4d09a9b2efabeea2f51d6dfdbffb1
Eric Quinton <eric.quinton@irstea.fr>
2017-06-12 15:07:56 +0200

    Modification du fichier index.php
    Suppression des scripts inutiles
    Ajout des tests de variables globales pour visualiser le contenu

--- index.php
+++ index.php
@@ -6,22 +6,13 @@
  * 
  * Attention : verifier la configuration de .htaccess, et notamment RewriteBase
  */
-if (isset($_GET["url"])) {
-	$url = split("/", $_GET["url"]);
-	if (is_numeric($url[1]) && ! isset($_REQUEST["id"])) {
-		$_GET["id"] = $url[1];
-		$_REQUEST["id"] = $url[1];
-	}
-	if (!isset($_REQUEST["module"])) {
-		if (strlen($url[0]) > 0) {
-			$_GET["module"] = $url[0];
-			if (strlen($url[2])> 0)
-				$_GET["module"] .= $url[2];
-		} else
-			$_GET["module"] = "default";
-			$_REQUEST["module"] = $_GET["module"];
-	}
-	//print_r($_GET);
-}
+
+echo "GET : ";
+print_r($_GET);
+echo "<br>SESSION : ";
+print_r($_SESSION);
+echo "<br>SERVER : ";
+print_r($_SERVER);
+
 include "framework/controller.php";
 ?>

a0fbca8cfecb93d92d3c9e50730dd7804b3c7443
cplumejeaud <cplumejeaud@gmail.com>
2017-06-13 15:15:14 +0200

    fixed the double quote for operation_version, and ADD COLUMN before.

--- install/col_alter-feature_metadata.sql
+++ install/col_alter-feature_metadata.sql
@@ -22,15 +22,15 @@ COMMENT ON COLUMN "sample_metadata"."data" IS 'Métadonnées en JSON';
 
 ALTER TABLE "operation" 
 ADD COLUMN metadata_form_id integer, 
-operation_version varchar,
+ADD COLUMN operation_version varchar,
 ADD COLUMN last_edit_date timestamp,
 ADD CONSTRAINT metadata_form_fk 
 FOREIGN KEY ("metadata_form_id")
 REFERENCES "metadata_form" ("metadata_form_id") MATCH SIMPLE
 ON UPDATE NO ACTION ON DELETE NO ACTION,
 ADD CONSTRAINT operation_name_version_unique UNIQUE (operation_name,operation_version);
-COMMENT ON COLUMN "operation"."operation_version" IS 'Version de l\'opération';
-COMMENT ON COLUMN "operation"."last_edit_date" IS 'Date de dernière édition de l\'opération';
+COMMENT ON COLUMN "operation"."operation_version" IS 'Version de l''opération';
+COMMENT ON COLUMN "operation"."last_edit_date" IS 'Date de dernière éditione l''opératon';
 
 ALTER TABLE "sample" ADD COLUMN sample_metadata_id integer, 
 ADD CONSTRAINT sample_metadata_fk 

a0d0f2481ccfe796f367472a7950569510ea8096
Eric Quinton <eric.quinton@irstea.fr>
2017-12-05 10:23:11 +0100

    param.default

--- param/param.default.inc.php
+++ param/param.default.inc.php
@@ -6,9 +6,9 @@
 * Parametres par defaut de l'application
 * Si des modifications doivent etre apportees, faites-les dans le fichier param.inc.php
 */
-$APPLI_version = "1.2.3";
+$APPLI_version = "1.2.4";
 $APPLI_dbversion = "1.2";
-$APPLI_versiondate = "22/11/2017";
+$APPLI_versiondate = "5/12/2017";
 $language = "fr";
 $DEFAULT_formatdate = "fr";
 /*

bd54a1c43b501b4e78334b76c36660de51a7207c
Eric Quinton <eric.quinton@irstea.fr>
2018-04-17 17:55:47 +0200

    Ajout du script de sauvegarde

---
+++ install/upgrade_by_psql.sql
@@ -0,0 +1,6 @@
+/*
+ * Collec-Science - 4/5/2018
+ * Script de modification de la base de donnees
+ * basculement de la version 1.2.3 a la version 2.0
+ * Execution de ce script en ligne de commande, en etant connecte root :
+ * su postgres -c "psql -f upgrade_by_psql.sql"

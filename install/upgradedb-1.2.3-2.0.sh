20e46e7f747203153e254055b8d3d56a4a9debf5
Eric Quinton <eric.quinton@irstea.fr>
2018-04-19 14:24:08 +0200

    Correction des scripts de maj

--- install/upgradedb-1.2.3-2.0.sh
+++ install/upgradedb-1.2.3-2.0.sh
@@ -2,7 +2,11 @@
 echo "have you a backup of your database and a copy of param/param.inc.php?"
 echo "Is your actual version of Collec-Science is 1.2.3?"
 read -p "Do you want to continue [y/n]?" answer
-if [ $answer = "y" ] then
-echo "update database"
+if [ $answer = "y" ]
+then
+echo "update database..."
+# command for upgrade when the database is in the same server than software
 su postgres -c "psql -f upgrade-1.2.3-2.0.sql"
+# command for upgrade when the database is hosted in another server than software
+# psql -U userlogin -h servername -f upgrade-1.2.3-2.0.sql
 fi

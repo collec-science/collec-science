a049c66987946b1f913d4a8171269825b3c52f25
Eric Quinton <eric.quinton@irstea.fr>
2018-06-12 18:04:14 +0200

    Ajout du support multi-langage pour les fichiers news-xx.txt

--- display/templates/documentation/quoideneuf.tpl
+++ display/templates/documentation/quoideneuf.tpl
@@ -1,2 +1,6 @@
 <h2>Les nouveautés...</h2>
+<div class="col-lg-8">
+<div class="row">
 {$texteNews}
+</div>
+</div>
--- doc/quoideneuf.php
+++ doc/quoideneuf.php
@@ -1,16 +1,38 @@
 <?php
-$fichier = file("param/news.txt");
-$doc = "";
-foreach ($fichier as $key => $value) {
-    if (substr($value, 1, 1) == "*" || substr($value, 0, 1) == "*") {
-        $doc .= "&nbsp;&nbsp;&nbsp;";
+file_exists("param/news-" . $LANG["date"]["locale"] . ".txt") ? $filename = "param/news-" . $LANG["date"]["locale"] . ".txt" : $filename = "param/news-fr.txt";
+/*
+ * lecture du fichier
+ */
+$f = fopen($filename, "r");
+if ($f) {
+    $doc = "";
+    $linebefore = "";
+    while ($line = fgets($f)) {
+        $line = trim($line);
+        if ($line == "News") {
+            $line = "";
+        }
+        
+        $firstchar = substr($line, 0, 1);
+        if ($firstchar == "=" || substr($line, 0, 2) == "--") {
+            if ($firstchar == "-") {
+                $linebefore = "<h3>" . $linebefore . "</h3>";
+            }
+        } else {
+            if (strlen($linebefore) > 0) {
+                $doc .= $linebefore . "<br>";
+            }
+            if (substr($line, - 1, 1) == ":") {
+                $linebefore = "<h4>" . htmlentities($line) . "</h4>";
+            } else {
+                $linebefore = htmlentities($line);
+            }
+        }
     }
-    if ($APPLI_utf8) {
-        utf8_encode($value);
-    }
-    $doc .= htmlentities($value) . "<br>";
+    $vue->set($doc, "texteNews");
+    $vue->set("documentation/quoideneuf.tpl", "corps");
+} else {
+    $message->set(_("Impossible de lire le fichier contenant les news"));
 }
 
-$vue->set($doc, "texteNews");
-$vue->set("documentation/quoideneuf.tpl", "corps");
 ?>

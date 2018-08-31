e6b16a4dfae6c0e11f2b56de7343da9132d6edda
Eric Quinton <eric.quinton@irstea.fr>
2018-08-31 15:53:21 +0200

    Ajout de l'affichage de la structure de la base des droits

--- framework/utils/structure.php
+++ framework/utils/structure.php
@@ -3,9 +3,10 @@
  * Recuperation de la structure de la base de donnees
  */
 require_once 'framework/utils/structure.class.php';
-$dataclass = new Structure($bdd);
+
 switch ($t_module["param"]) {
     case "html":
+        $dataclass = new Structure($bdd);
         $vue->set(
             $dataclass->generateHtml(
                 "tablename",
@@ -18,11 +19,25 @@ switch ($t_module["param"]) {
         $vue->set("dbstructure.tpl", "corps");
         break;
     case "latex":
+        $dataclass = new Structure($bdd);
         $vue->set(
             $dataclass->generateLatex(
                 "subsection",
                 "\\begin{tabular}{|l| p{2cm}|c|c|c| p{3cm}|}",
                 "\\end{tabular}"
+            ),
+            "data"
+        );
+        $vue->htmlVars[] = "data";
+        $vue->set("dbstructure.tpl", "corps");
+        break;
+    case "gacl":
+        $dataclass = new Structure($bdd_gacl);
+        $vue->set(
+            $dataclass->generateHtml(
+                "tablename",
+                "tablecomment",
+                "table table-bordered table-hover"
             ),
             "data"
         );

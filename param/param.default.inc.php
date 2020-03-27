963aa6ac06912cf2634cea6dc06e01d13ca142f8
Eric Quinton <eric.quinton@irstea.fr>
2020-03-27 15:14:18 +0100

    :release news: mise à jour de la liste des nouveautés

--- param/param.default.inc.php
+++ param/param.default.inc.php
@@ -6,9 +6,9 @@
 * Parametres par defaut de l'application
 * Si des modifications doivent etre apportees, faites-les dans le fichier param.inc.php
 */
-$APPLI_version = "2.4.0c";
+$APPLI_version = "2.4.0";
 $APPLI_dbversion = "2.4";
-$APPLI_versiondate = _("18/03/2020");
+$APPLI_versiondate = _("01/04/2020");
 $language = "fr";
 $DEFAULT_formatdate = "fr";
 /*

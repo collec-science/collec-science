02949e68d7331c90cccb4dbfa75bd1c832cf4497
Eric Quinton <eric.quinton@inrae.fr>
2020-08-25 17:39:58 +0200

    :feat utils: ajout d'un script permettant de tester un fichier xsl

---
+++ utils/testxsl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * test the configuration of a xsl file with data in a xml file
+ */
+if (empty ($argv[1]) || empty ($argv[2]) ) {
+  echo "usage: php testxsl.php file.xml transform.xsl";
+  die;
+}
+$xml = new DOMDocument;
+$xml->load($argv[1]);
+
+$xsl = new DOMDocument;
+$xsl->load($argv[2]);
+
+$proc = new XSLTProcessor;
+$proc->importStyleSheet($xsl);
+
+echo $proc->transformToXML($xml);

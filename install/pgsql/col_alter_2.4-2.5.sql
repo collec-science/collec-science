c57694b449354e01f2c84ba83a23f889ec401fdd
Eric Quinton <eric.quinton@inrae.fr>
2020-09-09 12:46:03 +0200

    :release v2.50c: préparation de la mise à jour

--- install/pgsql/col_alter_2.4-2.5.sql
+++ install/pgsql/col_alter_2.4-2.5.sql
@@ -362,7 +362,7 @@ INSERT INTO col.dataset_type (dataset_type_id, dataset_type_name, fields) VALUES
 alter table col.document add column 	uuid uuid NOT NULL DEFAULT gen_random_uuid();
 INSERT INTO col.export_model (export_model_name,pattern,target) 
 VALUES
-  ('export_template',cast('[{"tableName":"export_template","technicalKey":"export_template_id","isEmpty":false,"businessKey":"export_template_name","istable11":false,"booleanFields":["is_zipped"],"children":[{"aliasName":"export_dataset","isStrict":true}],"parameters":[],"istablenn":false},{"tableName":"export_dataset","isEmpty":false,"parentKey":"export_template_id","istable11":false,"booleanFields":[],"children":[],"parameters":[{"aliasName":"dataset_template","fieldName":"dataset_template_id"}],"istablenn":true,"tablenn":{"secondaryParentKey":"dataset_template_id","tableAlias":"dataset_template"}},{"tableName":"dataset_template","technicalKey":"dataset_template_id","isEmpty":true,"businessKey":"dataset_template_name","istable11":false,"booleanFields":[],"children":[{"aliasName":"dataset_column","isStrict":true}],"parameters":[],"istablenn":false},{"tableName":"dataset_column","technicalKey":"dataset_column_id","isEmpty":false,"parentKey":"dataset_template_id","istable11":false,"booleanFields":["mandatory"],"children":[],"parameters":[{"aliasName":"translator","fieldName":"translator_id"}],"istablenn":false},{"tableName":"translator","technicalKey":"translator_id","isEmpty":true,"businessKey":"translator_name","istable11":false,"booleanFields":[],"children":[],"parameters":[],"istablenn":false}]' as json),NULL);
+  ('export_template',cast('[{"tableName":"export_template","technicalKey":"export_template_id","isEmpty":false,"businessKey":"export_template_name","istable11":false,"children":[{"aliasName":"export_dataset","isStrict":true}],"parents":[],"istablenn":false},{"tableName":"export_dataset","isEmpty":false,"parentKey":"export_template_id","istable11":false,"children":[],"parents":[{"aliasName":"dataset_template","fieldName":"dataset_template_id"}],"istablenn":true,"tablenn":{"secondaryParentKey":"dataset_template_id","tableAlias":"dataset_template"}},{"tableName":"dataset_template","technicalKey":"dataset_template_id","isEmpty":true,"businessKey":"dataset_template_name","istable11":false,"children":[{"aliasName":"dataset_column","isStrict":true}],"parents":[],"istablenn":false},{"tableName":"dataset_column","technicalKey":"dataset_column_id","isEmpty":false,"parentKey":"dataset_template_id","istable11":false,"children":[],"parents":[{"aliasName":"translator","fieldName":"translator_id"}],"istablenn":false},{"tableName":"translator","technicalKey":"translator_id","isEmpty":true,"businessKey":"translator_name","istable11":false,"children":[],"parents":[],"istablenn":false}]' as json),NULL);
 
 -- ddl-end --
 
@@ -419,8 +419,6 @@ COMMENT ON COLUMN col.referent.academical_link IS E'Link used to identify the re
 
 update col.dataset_type set fields =  E'["uid","uuid","identifier","wgs84_x","wgs84_y","location_accuracy","object_status_name","referent_name","referent_email","address_name","address_line2","address_line3","address_city","address_country","referent_phone","referent_firstname","academic_directory","academic_link","object_comment","identifiers","sample_creation_date","sampling_date","multiple_value","sampling_place_name","expiration_date","sample_type_name","storage_product","clp_classification","multiple_type_name","collection_name","metadata","parent_uid","parent_uuid","parent_identifiers","web_address","content_type","container_uid","container_identifier","container_uuid","storage_type_name","fixed_value"]'
 where dataset_type_id = 1;
-update col.dataset_type set fields = E'["collection_name","collection_displayname","collection_keywords","referent_name","referent_firstname","academical_directory","academical_link","referent_email","address_name","address_line2","address_line3","address_city","address_country","referent_phone","fixed_value"]'
-where dataset_type_id = 2;
 
 CREATE TABLE col.samplesearch (
 	samplesearch_id serial NOT NULL,
@@ -447,5 +445,43 @@ ALTER TABLE col.samplesearch ADD CONSTRAINT collection_fk FOREIGN KEY (collectio
 REFERENCES col.collection (collection_id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE DEFERRABLE INITIALLY IMMEDIATE;
 
+-- Release 2.5.0c --
+CREATE TABLE col.license (
+	license_id serial NOT NULL,
+	license_name varchar NOT NULL,
+	license_url varchar,
+	CONSTRAINT license_pk PRIMARY KEY (license_id)
+
+);
+-- ddl-end --
+COMMENT ON TABLE col.license IS E'List of licenses usable to communicate informations on the collections';
+-- ddl-end --
+COMMENT ON COLUMN col.license.license_name IS E'Name of the license';
+-- ddl-end --
+COMMENT ON COLUMN col.license.license_url IS E'Link of download of the text of the license';
+-- ddl-end --
+ALTER TABLE col.license OWNER TO collec;
+-- ddl-end --
+
+INSERT INTO col.license (license_name, license_url) VALUES (E'CC0', E'https://creativecommons.org/publicdomain/zero/1.0/');
+-- ddl-end --
+INSERT INTO col.license (license_name, license_url) VALUES (E'CCBY', E'https://creativecommons.org/licenses/by/4.0/');
+-- ddl-end --
+
+-- object: license_id | type: COLUMN --
+-- ALTER TABLE col.collection DROP COLUMN IF EXISTS license_id CASCADE;
+ALTER TABLE col.collection ADD COLUMN license_id integer;
+-- ddl-end --
+
+-- [ Created foreign keys ] --
+-- object: license_fk | type: CONSTRAINT --
+-- ALTER TABLE col.collection DROP CONSTRAINT IF EXISTS license_fk CASCADE;
+ALTER TABLE col.collection ADD CONSTRAINT license_fk FOREIGN KEY (license_id)
+REFERENCES col.license (license_id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE DEFERRABLE INITIALLY IMMEDIATE;
+-- ddl-end --
+
+update col.dataset_type set fields = E'["collection_name","collection_displayname","collection_keywords","referent_name","referent_firstname","academical_directory","academical_link","referent_email","address_name","address_line2","address_line3","address_city","address_country","referent_phone","license_name","license_url","fixed_value"]'
+where dataset_type_id = 2;
 
 INSERT INTO col.dbversion (dbversion_number, dbversion_date) VALUES (E'2.5', E'2020-08-14');

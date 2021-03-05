c98a2d1336356d99cf8efcf1dc696a199b2b8d07
Eric Quinton <eric.quinton@inrae.fr>
2021-03-05 18:21:12 +0100

    :fix framework: correction du script de création de totp

--- install/pgsql/gacl_totp.sql
+++ install/pgsql/gacl_totp.sql
@@ -5,7 +5,7 @@ CREATE UNIQUE INDEX acllogin_login_idx ON gacl.acllogin
 	(
 	  login
 	);
-create sequence metabo.dbparam_dbparam_id_seq;
-select setval( 'metabo.dbparam_dbparam_id_seq', (select max(dbparam_id) from metabo.dbparam));
-alter table metabo.dbparam alter column dbparam_id set default nextval('metabo.dbparam_dbparam_id_seq');
-insert into metabo.dbparam (dbparam_name, dbparam_value) values ('otp_issuer', 'metabo');
+create sequence col.dbparam_dbparam_id_seq;
+select setval( 'col.dbparam_dbparam_id_seq', (select max(dbparam_id) from col.dbparam));
+alter table col.dbparam alter column dbparam_id set default nextval('col.dbparam_dbparam_id_seq');
+insert into col.dbparam (dbparam_name, dbparam_value) values ('otp_issuer', 'collec-science');

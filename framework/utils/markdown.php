25c9d6a03922b09967b4d417f8255be9a9540ec4
Eric Quinton <eric.quinton@inrae.fr>
2022-03-30 12:07:10 +0200

    :feat document: écriture de la doc sur les documents externes

--- framework/utils/markdown.php
+++ framework/utils/markdown.php
@@ -11,6 +11,9 @@ if (!file_exists($t_module["param"])) {
 }
 $handle = fopen($t_module["param"], "r");
 $content = fread($handle, filesize($t_module["param"]));
+if (strlen($content) == 0) {
+  $message->set(sprintf(_("Le fichier %s est vide ou ne peut être lu"), $t_module["param"], true));
+}
 $vue->set($content, "markdownContent");
 fclose($handle);
 $vue->set ("framework/utils/markdownDisplay.tpl", "corps");

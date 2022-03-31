50116527c798ab1e1dd2e35a7a0185a74c2dc7b4
Eric Quinton <eric.quinton@inrae.fr>
2022-03-31 12:36:10 +0200

    :feat document: suite corrections liées à l'affichage de la taille maxi
    issue #569

--- modules/gestion/sample.php
+++ modules/gestion/sample.php
@@ -197,7 +197,6 @@ switch ($t_module["param"]) {
     include_once 'modules/classes/document.class.php';
     $document = new Document($bdd, $ObjetBDDParam);
     $vue->set($document->getListFromField("uid", $data["uid"]), "dataDoc");
-    printA($document->getMaxUploadSize());
     $vue->set($document->getMaxUploadSize(), "maxUploadSize");
     /**
      * Get the list of authorized extensions

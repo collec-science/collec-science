cc5b6eb26979e27c3f9f0172102b72054eb355bf
Eric Quinton <eric.quinton@inrae.fr>
2022-08-25 14:56:03 +0200

    :feat framework: ajout d'une fonction de traitement des fichiers téléchargés

--- framework/fonctions.php
+++ framework/fonctions.php
@@ -553,3 +553,27 @@ function apiCall($method, $url, $certificate_path = "", $data = array(), $modeDe
   curl_close($curl);
   return $res;
 }
+
+/**
+ * Fonction permettant de reorganiser les donnees des fichiers telecharges,
+ * pour une utilisation directe en tableau
+ * @return multitype:multitype:NULL  unknown
+ */
+function formatFiles($attributName = "documentName")
+{
+    global $_FILES;
+    $files = array();
+    $fdata = $_FILES[$attributName];
+    if (is_array($fdata['name'])) {
+        for ($i = 0; $i < count($fdata['name']); ++$i) {
+            $files[] = array(
+                'name'    => $fdata['name'][$i],
+                'type'  => $fdata['type'][$i],
+                'tmp_name' => $fdata['tmp_name'][$i],
+                'error' => $fdata['error'][$i],
+                'size'  => $fdata['size'][$i]
+            );
+        }
+    } else $files[] = $fdata;
+    return $files;
+}

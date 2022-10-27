324a049a15ea9712349ce6f5d5ab19d696861326
Eric Quinton <eric.quinton@inrae.fr>
2022-10-27 16:05:40 +0200

    :feat framework: correction de l'affichage des variables systèmes

--- framework/utils/system.php
+++ framework/utils/system.php
@@ -2,14 +2,26 @@
 
 switch ($t_module["param"]) {
   case "SERVER":
-  $vue->set($_SERVER, "data");
-  $module_coderetour = 1;
-  break;
+    $vue->set($_SERVER, "data");
+    $module_coderetour = 1;
+    break;
   case "SESSION":
-  $vue->set($_SESSION, "data");
-  $module_coderetour = 1;
-  break;
+    $content = array();
+    foreach ($_SESSION as $k => $v) {
+      if (!is_object($v)) {
+        if (is_array($v)) {
+          $content[$k] = json_encode($v);
+        } else {
+        $content[$k] = $v;
+      }
+    }else {
+      $content[$k] = "object";
+    }
+  }
+    $vue->set($content, "data");
+    $module_coderetour = 1;
+    break;
   default:
-  $module_coderetour = -1;
+    $module_coderetour = -1;
 }
 $vue->set("framework/utils/system.tpl", "corps");

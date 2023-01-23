2a5c36b2a47b71d3874022052c694fa1d78fc633
Eric Quinton <eric.quinton@inrae.fr>
2023-01-23 14:47:58 +0100

    :rele scripts: mise à niveau des scripts de mise à jour

--- install/upgrade-2.4.0-2.8.2.sh
+++ install/upgrade-2.4.0-2.8.2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-OLDVERSION=collec-2.7.0
-VERSION=collec-2.8.1
+OLDVERSION=collec-2.4.0
+VERSION=collec-2.8.2
 REPO=https://github.com/collec-science/collec-science
 echo "Content of /var/www/html/collec-science"
 ls -l /var/www/html/collec-science
@@ -31,7 +31,6 @@ then
 unzip master.zip
 mv collec-science-master/ $VERSION
 
-
 # copy of last param into the new code
 cp collec/param/param.inc.php $VERSION/param/
 chgrp www-data $VERSION/param/param.inc.php
@@ -51,6 +50,9 @@ ln -s $VERSION collec
 echo "update database"
 chmod -R 755 /var/www/html/collec-science
 cd collec/install
+su postgres -c "psql -f upgrade-2.4-2.5.sql"
+su postgres -c "psql -f upgrade-2.5-2.6.sql"
+su postgres -c "psql -f upgrade-2.6-2.7.sql"
 su postgres -c "psql -f upgrade-2.7-2.8.sql"
 su postgres -c "psql -f upgrade-2.8-2.8.1.sql"
 cd ../..

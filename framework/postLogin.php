2caa9ddc907a7ecd42e4c02eef0e4c24af543e32
Eric Quinton <eric.quinton@inrae.fr>
2024-01-25 15:57:41 +0100

    :feat framework: ajout de l'affichage de la nouvelle version
    Interrogation curl vers Github

---
+++ framework/postLogin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Operations generated after login
+ */
+if ($_SESSION["droits"][$APPLI_release_right_minimal] == 1) {
+    /**
+     * Search from last release and display message if new release
+     */
+    include_once "framework/utils/getLastRelease.php";
+    $lastRelease = getLastRelease($APPLI_release_url, $APPLI_release_url_tag, $APPLI_release_url_date, $APPLI_release_user_agent);
+    if (!empty($lastRelease["tag"])) {
+        if ($APPLI_version != $lastRelease["tag"]) {
+            $date = date_create($lastRelease["date"]);
+            $message->set(sprintf(_("La nouvelle version %1s du %2s a été publiée !"), $lastRelease["tag"], date_format($date, $_SESSION["MASKDATE"])));
+        }
+    }
+}

0fd3108d2f0e15aa9595d6be3d1b63aefb9c656e
Eric Quinton <eric.quinton@proton.me>
2025-07-21 18:39:26 +0200

    :rele v25.0.10: préparation de la nouvelle version

--- app/Config/App.php
+++ app/Config/App.php
@@ -182,13 +182,13 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public string $version = "v25.0.9";
+    public string $version = "v25.0.10";
     /**
      * versionDate - don't change here, but in function setParameters() below
      *
      * @var string
      */
-    public string $versionDate = "12/06/2025";
+    public string $versionDate = "25/07/2025";
     /**
      * Number of the database version
      *
